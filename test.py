290b30104c4d65a4091744042e22dc8c140a7c69
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-13 08:42:32 +0900

    age check for job search

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-print("hello world")
+print("hello world!")
 
-age=int(input("enter age"))
+age=int(input("your age?"))
+# normal print statement
+print("your age as entered "+str(age))
+# formated string literals
+print(f"your age is {age}")
 
-print(age)
+if age>=18:
+    print("you can search for jobs")
+else:
+    print("wait ... finish your studies")
