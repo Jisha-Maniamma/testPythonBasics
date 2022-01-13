4077116c486a471d348fd48fa3e39af51eb80fdb
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-14 07:57:21 +0900

    Love percentage calculator

--- LovePercentCalculator.py
+++ LovePercentCalculator.py
@@ -15,12 +15,12 @@ r= fullname.count('r')
 u= fullname.count('u')
 e= fullname.count('e')
 score=str(t+r+u+e)+str(l+o+v+e)
-print(f" {first_name} and {second_name} , your compatibility is {score}")
+print(f"{first_name.upper()} and {second_name.upper()} , your compatibility is {score}")
 
 score=int(score)
 if score<10 or score>90:
-    print()
-elif score>=40 or score<=50:
     print(f"you score is {score}, you go together like coke and mentos")
+elif 40 <= score <= 50:
+    print(f"you score is {score}, you are alright together")
 else:
-    print(f"you score is {score}, you are alright togethe")
+    print(f"you score is {score}")
