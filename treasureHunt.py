499586192c3dcb184dd8369940a776805c6f0b6a
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-14 13:35:12 +0900

    Treasure Hunt

--- treasureHunt.py
+++ treasureHunt.py
@@ -1,5 +1,27 @@
-print("welcome to Treasure island. Your mission is to find treasure")
-direction_choice = input("choose left or right? \n").lower()
+print('''
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+            jgs '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+''')
+print("Welcome to Treasure Island.")
+print("You\'r mission is to find the treasure.")
+direction_choice = input("choose 'left' or 'right'? \n").lower()
 if direction_choice == 'right':
     print("Game Over")
 else:
