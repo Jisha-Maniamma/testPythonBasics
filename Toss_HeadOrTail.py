a4fc9bb07d97a94044acee82160ea9439d36f817
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-17 07:48:02 +0900

    Tossing for Head/Tail

---
+++ Toss_HeadOrTail.py
@@ -0,0 +1,8 @@
+import random;
+
+print("Starting to toss...")
+random_number = random.randint(0, 1);
+if random_number == 1:
+    print("you got a Head...you Win!!")
+else:
+    print("you got a Tail...you loose!")
