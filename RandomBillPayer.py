eb50963ae6a40ef649b5a1c0a237332ce2b1b112
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-17 07:35:35 +0900

    Random friend selection

--- RandomBillPayer.py
+++ RandomBillPayer.py
@@ -1,4 +1,5 @@
 import random;
+# import treasureHunt;
 # To print randoms friends among friends who had dinner together and this friend will pay th bills of
 # all the other friends too
 names = input("Enter names of friends who had dinner [seperate them by a -,]").split(",")
@@ -6,4 +7,5 @@ random_Number = random.randint(0, len(names));
 
 # print(names)
 
+# print("name of restaurant is "+treasureHunt.Restaurant_name)
 print(f"th randomly selected friend who would pay bill is- {names[random_Number]}")
