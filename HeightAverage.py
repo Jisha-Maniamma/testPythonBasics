3dce3eb732f8fd093da16ede5da07516a89dede3
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-18 07:32:25 +0900

    Avegae height among a list of heights of students

--- HeightAverage.py
+++ HeightAverage.py
@@ -1,16 +1,17 @@
-
 heights = input("Enter all the students heights (seperated by a space)\n").split()
 
 for i in range(0, len(heights)):
     heights[i] = int(heights[i])
 print(heights)
 
-total_height=0
-total_length=0
+total_height = 0
+total_length = 0
 for height in heights:
-    total_height+=height
-    total_length+=1
+    total_height += height
+    total_length += 1
 
-print(f"average height is {str(round(total_height/total_length))}")
+# using traditional for loop
+print(f"average height is {str(round(total_height / total_length))}")
 
-
+# using sum() and len() function
+print(f"average height is {str(round(sum(heights) / len(heights)))}")
