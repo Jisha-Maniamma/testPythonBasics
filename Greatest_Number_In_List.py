fe63dc5e24c61ba5c3887937a353196e585ec6e8
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-18 07:51:17 +0900

    finding large number

---
+++ Greatest_Number_In_List.py
@@ -0,0 +1,13 @@
+number = input("Enter the numbers [with space]\n").split()
+
+for i in range(0, len(number)):
+    number[i] = int(number[i])
+
+print(number)
+max_number = 0;
+for num in number:
+    print(f"{max_number} and {num}")
+    if max_number < num:
+        max_number = num
+
+print(f"Maximum number is {max_number}")
