279a80cba9307f8930a1f5b144e38e9aa94d0c10
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-21 08:09:57 +0900

    Hangman Game

--- HangMan_Game.py
+++ HangMan_Game.py
@@ -5,11 +5,14 @@ words=["Business","Portfolio","Adjustment"]
 
 random_Selected_Word=random.choice(words)
 To_fill_word=[]
+To_fill=""
 for i in range(len(random_Selected_Word)):
     To_fill_word.append("_")
+    To_fill+="_ "
 
-# print(random_Selected_Word)
+
+# print(To_fill)
 
 
-print(f"Kindly start guessing for {To_fill_word}")
+print(f"Kindly start guessing for {To_fill}")
 # UserInput=print("Enter a character to guess")
