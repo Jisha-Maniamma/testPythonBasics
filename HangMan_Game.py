43a11381c12bdd13e626d0a94d58497280dfbe25
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-24 07:48:15 +0900

    Hangman Game

--- HangMan_Game.py
+++ HangMan_Game.py
@@ -1,24 +1,25 @@
 import random
-print("Welcome to Hangman game!")
-#business portfolio
-words=["bus","port","adjustment"]
 
-random_Selected_Word=random.choice(words)
-To_fill_word=[]
-To_fill=""
+print("Welcome to Hangman game!")
+# business portfolio
+words = ["buus", "poprt", "adjustament"]
+
+random_Selected_Word = random.choice(words)
+To_fill_word = []
+To_fill = ""
 for i in range(len(random_Selected_Word)):
     To_fill_word.append("_")
-    To_fill+="_ "
-
+    To_fill += "_ "
 
 # print(To_fill)
 
 
-print(f"Kindly start guessing for {To_fill}")
-UserInput=input("Enter a character to guess").lower()
+print(f"Kindly start guessing for {To_fill} {To_fill_word}")
+UserInput = input("Enter a character to guess").lower()
 
-for char in random_Selected_Word:
-    if(UserInput==char):
-        print("correct")
-    else:
-        print("wong")
+for position in range(len(random_Selected_Word)):
+    if random_Selected_Word[position] == UserInput:
+        To_fill_word[position] = UserInput
+    # print(f"correct  ")
+
+print(f"Kindly start guessing for  {To_fill_word}")
