7d442c6fd4a2bb1dc8ee420d807a7e4650979ea9
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-26 07:41:29 +0900

    Learn functions

--- functions/function_learn1.py
+++ functions/function_learn1.py
@@ -5,3 +5,7 @@ def greet(name, age, height):
 for i in range(0, 3):
     name = "a" + str(i)
     greet(name=name, height=150 + i, age=(20 + i))
+
+# hai there a0 with age- 20 and height 150, this is jisha!
+# hai there a1 with age- 21 and height 151, this is jisha!
+# hai there a2 with age- 22 and height 152, this is jisha!
