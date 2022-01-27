a554e10a66727a0829687bb19c3e4bbad09b84a4
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-28 07:38:16 +0900

    Caesar Cipher

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -8,9 +8,11 @@ shift = int(input("Type the shift number:\n"))
 
 def Caesar_Cipher(direction, text, shift):
     if direction == "encode":
+        cipherText=""
         for a in text:
-            print(f"{alphabet.index(a)}{shift} {int(int(alphabet.index(a)) + shift)}")
-            print(f"{alphabet.pop(alphabet.index(a) + shift)}")
+            # print(f"at location {alphabet.index(a)}  new location{int(int(alphabet.index(a)) + shift)}")
+            # print(f" new charactpr for {a} -> {alphabet[alphabet.index(a) + shift]}")
+            cipherText+=alphabet[alphabet.index(a) + shift]
 
 
 if direction == "encode" or direction == "decode":
