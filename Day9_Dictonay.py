dbbed7cb494c8f5cf5f2876c8a808fd7d8d841f7
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-02 07:27:52 +0900

    Dictionary in Python

--- Day9_Dictonay.py
+++ Day9_Dictonay.py
@@ -1,10 +1,12 @@
-proramming_Dicitonary={
-    "Bug":"An error",
-    "Function":"a piece of code"
+proramming_Dicitonary = {
+    "Bug": "An error",
+    "Function": "a piece of code"
 
 }
 
 print(proramming_Dicitonary["Bug"])
 
-proramming_Dicitonary["loop"]="over and over again"
+proramming_Dicitonary["loop"] = "over and over again"
 print(proramming_Dicitonary)
+
+a = {}
