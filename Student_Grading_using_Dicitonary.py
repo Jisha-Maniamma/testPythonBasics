69bed7c07563feaaac1463d8cf934aa6c8b5d373
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-02 07:39:01 +0900

    students grading using Dictionary in Python

--- Student_Grading_using_Dicitonary.py
+++ Student_Grading_using_Dicitonary.py
@@ -1,6 +1,20 @@
-studnt_score={
-    "Harry":81,
-    "James":79,
-    "Krishna":100,
-"jisha":30
+student_score = {
+    "Harry": 81,
+    "James": 79,
+    "Krishna": 100,
+    "Misha": 30
 }
+
+student_grades={}
+for students in student_score:
+    if student_score[students]>90:
+        student_grades[students]="outstanding"
+    elif student_score[students]>80:
+        student_grades[students]="exceeds Expectation"
+    elif student_score[students]>80:
+        student_grades[students]="acceptable"
+    else:
+        student_grades[students]="fail"
+
+print(student_grades)
+
