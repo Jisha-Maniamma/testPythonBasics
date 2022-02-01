2eedad185660c8e9ba1130fd9fb18e048c1b0978
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-02 08:17:28 +0900

    understanding Dictionary in Python

---
+++ facebook_info_dictornaries.py
@@ -0,0 +1,9 @@
+users=[{"name":"Mohan"},{"name":"James"}]
+
+
+def addUser(name, age, friendsNumber, friendsName, unreadMeassages):
+    user_info= {"name": name, "age": age, "friendsNumber": friendsNumber, "friendsName": friendsName,
+                "unreadMeassages": unreadMeassages}
+
+
+addUser(name="Ron",age=12,friendsNumber=100,friendsName=["aji","shyam","rahul","Raju"],unreadMeassages=2)
