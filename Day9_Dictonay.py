0dbb5850accba66b09009a112bb639272288451e
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-02 07:29:48 +0900

    Dictionary in Python

--- Day9_Dictonay.py
+++ Day9_Dictonay.py
@@ -5,8 +5,14 @@ proramming_Dicitonary = {
 }
 
 print(proramming_Dicitonary["Bug"])
-
+######## add new entry and change entry
 proramming_Dicitonary["loop"] = "over and over again"
+proramming_Dicitonary["Bug"] = "again and again"
 print(proramming_Dicitonary)
 
 a = {}
+proramming_Dicitonary={}
+print(proramming_Dicitonary)
+
+proramming_Dicitonary["Bug"] = "again and again"
+print(proramming_Dicitonary)
