d5514b670970181a52f4ddc34ce16d0faf2f302a
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-02 08:34:56 +0900

    understanding Dictionary in Python

--- facebook_info_dictornaries.py
+++ facebook_info_dictornaries.py
@@ -1,4 +1,19 @@
-users = [{"name": "Mohan"}, {"name": "James"}]
+users = [
+    {
+        "name": "Mohan",
+        "age":0,
+        'friendsNumber': 0,
+        'friendsName': [],
+        'unreadMeassages': 0
+    },
+    {
+        "name": "James",
+        "age":0,
+        'friendsNumber': 0,
+        'friendsName': [],
+        'unreadMeassages': 0
+    }
+]
 
 
 def addUser(name, age, friendsNumber, friendsName, unreadMeassages):
@@ -16,14 +31,14 @@ def updateUser(name, age, friendsNumber, friendsName, unreadMeassages):
         for a in thing:
             if thing[a] == name:
                 thing["age"] = age
-                # thing["friendsNumber"]=friendsNumber
-                # thing["friendsName"]=friendsName
-                # thing["unreadMeassages"]=unreadMeassages
-                print(thing["age"])
+                thing["friendsNumber"]=friendsNumber
+                thing["friendsName"]=friendsName
+                thing["unreadMeassages"]=unreadMeassages
+
 
 
 addUser(name="Ron", age=12, friendsNumber=100, friendsName=["aji", "shyam", "rahul", "Raju"], unreadMeassages=2)
 print(users)
 print("\n\n")
-updateUser(name="James", age=12, friendsNumber=100, friendsName=["aji", "shyam", "rahul", "Raju"], unreadMeassages=2)
+updateUser(name="Mohan", age=12, friendsNumber=100, friendsName=["aji", "shyam", "rahul", "Raju"], unreadMeassages=2)
 print(users)
