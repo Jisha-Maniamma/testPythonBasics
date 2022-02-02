83edce135f767107e4f5169a59a7145249e70c4b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-03 08:31:08 +0900

    Blind Bidding using Dictionary in Python

--- Blind_Auction_.py
+++ Blind_Auction_.py
@@ -1,42 +1,44 @@
-
 from os import system
-bidderInfo=[]
+
+bidderInfo = []
+
+
 def addBiddingInfo(name, price):
-    bidderInfo_new={}
-    bidderInfo_new["name"]=name
-    bidderInfo_new["price"]=price
+    bidderInfo_new = {}
+    bidderInfo_new["name"] = name
+    bidderInfo_new["price"] = price
+    # bidderInfo_new = {"name": name, "price": price}
     bidderInfo.append(bidderInfo_new)
 
-print("logo............................")
-continuloop=True
-while(continuloop):
-    name=input("Enter the bidder name ")
-    price=int(input("enetr the price"))
-    addBiddingInfo(name=name,price=price)
-    otherUser=input("type 'yes' if some one wants to also bid, else type 'no' ")
-    continuloop=True if(otherUser=='yes') else False
 
-#add to dictonary
+print("logo............................")
+continuloop = True
+while (continuloop):
+    name = input("Enter the bidder name ")
+    price = int(input("enetr the price"))
+    addBiddingInfo(name=name, price=price)
+    otherUser = input("type 'yes' if some one wants to also bid, else type 'no' ")
+    continuloop = True if (otherUser == 'yes') else False
+
+
+# add to dictonary
 
 def getWinner():
-    greatest=0
-    winnername=""
+    greatest = 0
+    winnername = ""
     for i in range(len(bidderInfo)):
-       if(bidderInfo[i]["price"]>greatest):
-           greatest=bidderInfo[i]["price"]
-           winnername=bidderInfo[i]["name"]
+        if (bidderInfo[i]["price"] > greatest):
+            greatest = bidderInfo[i]["price"]
+            winnername = bidderInfo[i]["name"]
     print(f"{greatest} {winnername}")
 
 
 if not continuloop:
     getWinner()
 
-
 print(bidderInfo)
 
-
-
-#ask if anyon else
+# ask if anyon else
 #########if ys clear screen if no find highest bid and show
 
 # system("clear")
