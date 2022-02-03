67889e5944dd2829827932df51ee1349ebdc7f23
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-04 08:27:03 +0900

    return type functions in Python

--- functions/Capitalize_name.py
+++ functions/Capitalize_name.py
@@ -1,11 +1,13 @@
 #   Program to capitaliz ach starting word of your name
 Firstname = input("Enter your first name  ").lower()
 Lastname = input("Enter our last name  ").lower()
-MiddleName = input("Enter our middle name  ").lower()
+
 
 
 def capitalize(Firstname, midddleName,Lastname):
-    return Firstname[0].upper() + Firstname[1:len(Firstname)] + " " + Lastname[0].upper() + Lastname[1:len(Lastname)]
+    return Firstname[0].upper() + Firstname[1:len(Firstname)] \
+           + " " + midddleName[0].upper() + midddleName[1:len(midddleName)]\
+           + " " + Lastname[0].upper() + Lastname[1:len(Lastname)]
 
 
 print(capitalize(Firstname,MiddleName, Lastname))
