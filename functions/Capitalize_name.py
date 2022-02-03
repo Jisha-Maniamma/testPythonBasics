c08247968981ae694d14dfbbfbcadda66f47fda9
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-04 08:21:46 +0900

    return type functions in Python

---
+++ functions/Capitalize_name.py
@@ -0,0 +1,10 @@
+Firstname=input("Enter your first name  ").lower()
+Lastname=input("Enter our last name  ").lower()
+
+
+def capitalize(Firstname,Lastname):
+
+    return Firstname[0].upper()+Firstname[1:len(Firstname)]+" "+Lastname[0].upper()+Lastname[1:len(Lastname)]
+
+
+print(capitalize(Firstname,Lastname))
