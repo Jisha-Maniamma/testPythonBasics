997c5b9b52dd352782661e8b9acc6b95e917a978
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-07 08:05:57 +0900

    Calculator in Python

--- Calculator.py
+++ Calculator.py
@@ -24,11 +24,18 @@ operator["/"]=div
 
 num1=int(input("Enter the first number "))
 num2=int(input("Enter the Second number "))
-print("which of the opertion do you wnt to do")
-for op in operator:
-    print(op)
-
-
-symbol_selected=input("select the symbol  ")
-calculator=operator[symbol_selected]
-print(f"{calculator(num1,num2)}")
+choice="y"
+while choice=="y":
+    print("which of the operation do you wnt to do")
+    for op in operator:
+        print(op)
+    symbol_selected=input("select the symbol  ")
+    calculator=operator[symbol_selected]
+    answer=calculator(num1,num2)
+    print(f"{num1}{symbol_selected}{num2}={answer}")
+    choice=input("type 'y' to continue using the last result")
+    if choice!='y':
+        print("exiting..")
+    else:
+        num1=answer
+        num2=int(input("Enter the Second number "))
