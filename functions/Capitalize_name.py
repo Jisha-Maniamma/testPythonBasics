ab937913de03e9ff8bd424ab96b725c5b3965b13
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-07 07:22:05 +0900

    return type functions in Python

--- functions/Capitalize_name.py
+++ functions/Capitalize_name.py
@@ -6,7 +6,7 @@ Lastname = input("Enter our last name  ").lower()
 
 def capitalize(firstname, midddlename, lastname):
     if firstname == "" and midddlename == "" and lastname == "":
-        return "yor didnot provide valid inputs!"
+        return "you didnot provide valid inputs!"
     return firstname[0].upper() + firstname[1:len(firstname)] \
            + (" " + midddlename[0].upper() + midddlename[1:len(midddlename)] if midddlename != "" else "") \
            + " " + lastname[0].upper() + f"{lastname[1:len(lastname)]}"
