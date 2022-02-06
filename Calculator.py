cc740dcff81544201daeb1db38e05d96972f6f5a
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-07 08:07:53 +0900

    Calculator in Python

--- Calculator.py
+++ Calculator.py
@@ -14,28 +14,24 @@ def div(n1, n2):
     return n1 / n2
 
 
-operator={}
-operator["+"]=add
-operator["-"]=sub
-operator["*"]=mul
-operator["/"]=div
+operator = {"+": add, "-": sub, "*": mul, "/": div}
 
 # print(operator)
 
-num1=int(input("Enter the first number "))
+num1 = int(input("Enter the first number "))
 
-choice="y"
-while choice=="y":
-    num2=int(input("Enter the Second number "))
+choice = "y"
+while choice == "y":
+    num2 = int(input("Enter the Second number "))
     print("which of the operation do you wnt to do")
     for op in operator:
         print(op)
-    symbol_selected=input("select the symbol  ")
-    calculator=operator[symbol_selected]
-    answer=calculator(num1,num2)
+    symbol_selected = input("select the symbol  ")
+    calculator = operator[symbol_selected]
+    answer = calculator(num1, num2)
     print(f"{num1}{symbol_selected}{num2}={answer}")
-    choice=input("type 'y' to continue using the last result")
-    if choice!='y':
+    choice = input("type 'y' to continue using the last result")
+    if choice != 'y':
         print("exiting..")
     else:
-        num1=answer
+        num1 = answer
