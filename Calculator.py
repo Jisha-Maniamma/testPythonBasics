799dd5020eb37a3c0a85ea6f71a4f8656577d260
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-07 07:50:44 +0900

    Calculator in Python

--- Calculator.py
+++ Calculator.py
@@ -20,4 +20,15 @@ operator["-"]=sub
 operator["*"]=mul
 operator["/"]=div
 
-print(operator)
+# print(operator)
+
+num1=int(input("Enter the first number "))
+num2=int(input("Enter the Second number "))
+print("which of the opertion do you wnt to do")
+for op in operator:
+    print(op)
+
+
+symbol_selected=input("select the symbol  ")
+calculator=operator[symbol_selected]
+print(f"{calculator(num1,num2)}")
